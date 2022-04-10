d7cb3beb7fa5a8503c47a8445deed443a733ec26
Pavel Kilbas <pavelkilbas@gmail.com>
2022-04-10 19:28:42 +0300

    added routers

--- server/index.js
+++ server/index.js
@@ -6,12 +6,20 @@ const sequelize = require('./db')
 
 const  models = require('./models/models')
 const cors = require('cors')
+const router =  require('./routes/index')
+
+
+
 const PORT = process.env.PORT
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use('/api',router)
+
 app.get('/',(req, res) => {
+
 
     res.status(200).json({message:'Working!'})
 
