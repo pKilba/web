7204fc993f9fe9daa5cd06b7e2b433f3a88bb64b
Pavel Kilbas <pavelkilbas@gmail.com>
2022-04-17 18:25:52 +0300

    added functional server

--- server/index.js
+++ server/index.js
@@ -7,24 +7,25 @@ const sequelize = require('./db')
 const  models = require('./models/models')
 const cors = require('cors')
 const router =  require('./routes/index')
+const fileUpload = require('express-fileupload')
+
+
+const errorHandler =  require('./middleware/ErrorHandlingMiddleware')
 
 
 
 const PORT = process.env.PORT
+const path =require('path')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname,'static')))
+app.use(fileUpload({}))
 
 app.use('/api',router)
 
-app.get('/',(req, res) => {
-
-
-    res.status(200).json({message:'Working!'})
-
-
-})
+app.use(errorHandler)
 
 const start = async () => {
     try {
